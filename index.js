682ed004c4a6e075bdd5441c3447bc98b0462086
Alex Hoyau <a.hoyau@silexlabs.org>
2018-09-17 16:27:25 +0200

    allow search untill / because of heroku which has a working folder in /app

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ const fs = require('fs');
 const pwd = __dirname.split(path.sep);
 
 function getPath(moduleName, folder = pwd) {
-  if(folder.length < 2) return null;
+  if(folder.length < 1) {
+    logError(nodeModuleName, folder);
+    return null;
+  }
   const nodeModulesPath = folder.concat(['node_modules']).join(path.sep);
   if(fs.existsSync(nodeModulesPath + '/' + (moduleName || ''))) {
     return nodeModulesPath;
   }
-  //console.log('not found:', nodeModulesPath + '/' + (moduleName || ''));
-  return getPath(moduleName, folder.slice(0, -1));
+  res = getPath(moduleName, folder.slice(0, -1));
+  if(!res) logError(nodeModuleName, folder);
+  return res;
 };
 
+function logError(nodeModuleName, folder) {
+  console.error(`Could not find the node_modules folder ${ moduleName ? "which contains " + moduleName : "" } in ${ folder.join('/') }`);
+}
 const result = getPath(process.argv[2]);
 
 // set an env var - avail to the current process, not in the global shell process
