837fc66a527c4cbc7405dd8916e74a53e66c3108
Alex Hoyau <a.hoyau@silexlabs.org>
2019-05-15 12:12:31 -0500

    better unit tests

--- test.js
+++ test.js
@@ -2,25 +2,66 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
-const dest = './tests/node_modules/module1/';
-const fileName = 'index.js';
-console.log('Test: move the script to', dest);
+// test function
+const srcFile = 'index.js';
+function testFromLocation(dest, moduleName) {
+  // console.log('Test: move the script to', dest);
+  const destModFolder = path.join(dest, 'node_modules-path');
+  const destFile = path.join(destModFolder, srcFile);
+  try {
+    fs.mkdirSync(destModFolder);
+    fs.copyFileSync(srcFile, destFile);
+  }
+  catch(e) {}
+  const node_modules = require(destFile);
+  return node_modules(moduleName);
+}
 
-fs.copyFileSync(fileName, dest + fileName);
-const node_modules = require(dest + fileName);
+// init folders
+const testsFolder = path.resolve(__dirname, 'tests');
+const node_modules0 = path.resolve(__dirname, 'tests/node_modules');
+const node_modules1 = path.resolve(__dirname, 'tests/node_modules/module1/node_modules');
+const node_modules2 = path.resolve(__dirname, 'tests/node_modules/module1/node_modules/module2/node_modules');
+fs.mkdirSync(testsFolder);
+fs.mkdirSync(node_modules0);
+fs.mkdirSync(path.resolve(node_modules0, 'module1'));
+fs.mkdirSync(node_modules1);
+fs.mkdirSync(path.resolve(node_modules1, 'module2'));
+fs.mkdirSync(node_modules2);
 
-
-const pwd = __dirname;
-const node_modules1 = path.resolve(__dirname, 'tests/node_modules');
-const node_modules2 = path.resolve(__dirname, 'tests/node_modules/module1/node_modules');
-
-assert.equal(node_modules(), node_modules2);
-assert.equal(node_modules('module1'), node_modules1);
-assert.equal(node_modules('module2'), node_modules2);
+// tests
+try {
+  assert.equal(node_modules0, testFromLocation(node_modules0, 'module1'));
+  assert.equal(node_modules0, testFromLocation(node_modules1, 'module1'));
+  assert.equal(node_modules1, testFromLocation(node_modules1, 'module2'));
+  assert.equal(node_modules0, testFromLocation(node_modules2, 'module1'));
+  assert.equal(node_modules1, testFromLocation(node_modules2, 'module2'));
+}
+catch(e) {
+  console.error(e.message);
+  console.error('-------------');
+  cleanup();
+  throw e;
+}
 console.log('Test success');
 
-console.log('Cleanup: remove', dest + fileName);
-fs.unlinkSync(dest + fileName);
-
-console.log('Done');
-
+// cleanup
+function rmdir(path) {
+  if (fs.existsSync(path)) {
+    fs.readdirSync(path).forEach(function(file, index){
+      var curPath = path + "/" + file;
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        rmdir(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
+};
+function cleanup() {
+  console.log('Cleanup: remove tests/');
+  rmdir(path.resolve(__dirname, 'tests'));
+  console.log('Done');
+}
+cleanup();
